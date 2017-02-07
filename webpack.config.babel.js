a17f594bde6a875137076c24d875906ecd67c9e3
zarkoselak <zarko.selak@trikoder.net>
2017-02-07 12:46:50 +0100

    adds webpack config

---
+++ webpack.config.babel.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import webpack from 'webpack';
+
+export default {
+  entry: './src/index',
+  output: {
+    filename: 'dist/index.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ]
+};
