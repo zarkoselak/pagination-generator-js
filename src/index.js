f83408b1f188083a57052f9b069e76fef3ad15a7
zarkoselak <zarko.selak@trikoder.net>
2017-02-08 13:18:11 +0100

    update
    - adds new logic for pagination rendering
    - removes full navigation render
    - adds separators

--- src/index.js
+++ src/index.js
@@ -1,46 +1,57 @@
 'use strict';
 
 /**
- * Generate pagination Array
- * @param  {Object} data Object with number of total elements and elements per page
+ * Generates pagination Array
+ * @param  {Object} data Object with number of total elements, elements per page and current page
  * @return {Array}      Pagination array
  */
 export const generate = (data) => {
-  const { total, page }           = data;
-  const maxPagesForFullPagination = 10
-  const pages                     = Math.ceil(total/page);
+  const { total, page, selected = 1}  = data;
+  const pages = Math.ceil(total/page);
 
-  let pagination = [];
-
-  if(pages < maxPagesForFullPagination) {
-    pagination = generateFullPagination(pages);
-  } else {
-    pagination = generateShortPagination(pages);
-  }
-  return pagination;
+  return generatePagination(pages, selected);
 };
+
 
 /**
- * generates full pagination array
- * @param  {Number} pages number of total pages
- * @return {Array}       pagination array
+ * Adds separator
+ * @param {Number} selected current page
+ * @param {Number} page     first or last page
+ * @param {Number} offset   left or right offset from current page
  */
-const generateFullPagination = (pages) => {
-  return Array.from({length: pages}, (v,i) => i+1);
-};
+const addSeparator = (selected, page, offset) => {
+  const separator = `...`;
+  return Math.abs(page - selected) > offset ? separator : null;
+}
 
 /**
  * generates short pagination array with first 10 elements, placeholder for middElements and last element
  * @param  {Number} pages number of total pages
  * @return {Array}        pagination array
  */
-const generateShortPagination = (pages) => {
-  const maxNumberOfElements = 10;
+const generatePagination = (pages, selected) => {
+  const maxNumberOfElements = 5;
+  const firstPage           = 1;
+  const lastPage            = pages;
+  const offset              = Math.ceil(maxNumberOfElements / 2);
+  const offsetLeft          = [...Array(offset).keys()].map((value, index) => Math.abs(value - selected) <= firstPage || Math.abs(value - selected) === selected ? null : Math.abs(value - selected)).reverse();
+  const offsetRight         = [...Array(offset).keys()].map((value, index) => (selected + value) >= lastPage || (selected + value) === selected ? null : (selected + value));
 
-  let pagination = [];
-  let startElements = Array.from({length: maxNumberOfElements}, (v,i) => i+1);
-  let middElements  = `...`;
-  let lastElement   = [pages];
+  let pagination = [
+    firstPage,
+    addSeparator(selected, firstPage, offset),
+    ...offsetLeft,
+    selected !== firstPage && selected !== lastPage ? selected: null,
+    ...offsetRight,
+    addSeparator(selected, lastPage, offset),
+    lastPage
+  ];
 
-  return [...startElements, middElements, ...lastElement];
+  return pagination.filter(item => item !== null)
+  .map((item, index, items) => {
+    return {
+      page: item,
+      isSelected: item === selected
+    };
+  });
 };
